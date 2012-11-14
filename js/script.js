7853da09c87c6f8b6a836803586d3aaa6c82a7f9
Alexander Paz <alexjpaz@gmail.com>
2012-11-14 11:11:36 -0500

    Removed contact info

--- js/script.js
+++ js/script.js
@@ -13,26 +13,26 @@ var ajpaz = {
 //		});
         
 		
-		var submit_handler = function() {
-			$('#contact-submit').val('Sending').attr('disabled', true).addClass('sending');
-			var values = $('#footer-contact').serialize();
-			$.ajax({
-			   type: "POST",
-			   url: "send.php",
-			   data: values,
-			   success: function(msg){
-				 $('#contact-submit').val('Thanks!').attr('disabled', true).addClass('sent').removeClass('sending');
-			     setTimeout($.colorbox.close, 1000);
-			   }
-			 });
-			 return false;
-		};
+//		var submit_handler = function() {
+//			$('#contact-submit').val('Sending').attr('disabled', true).addClass('sending');
+//			var values = $('#footer-contact').serialize();
+//			$.ajax({
+//			   type: "POST",
+//			   url: "send.php",
+//			   data: values,
+//			   success: function(msg){
+//				 $('#contact-submit').val('Thanks!').attr('disabled', true).addClass('sent').removeClass('sending');
+//			     setTimeout($.colorbox.close, 1000);
+//			   }
+//			 });
+//			 return false;
+//		};
 		
-		$('#footer-contact').validate({
-		 submitHandler: function(form) {
-		   submit_handler();;
-		 }
-		});
+//		$('#footer-contact').validate({
+//		 submitHandler: function(form) {
+//		   submit_handler();;
+//		 }
+//		});
 	}
 }
 
