ff6b71ab26cdc7e969c6c8cec15404595d6bb003
Alex Paz <alexander.j.paz@ngc.com>
2013-02-25 09:02:30 -0500

    Added google analytics

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,9 @@
 (function(){
-	
+	// Any other scripts loaded here
+
+	// Google Analytics
+	head.js("http://www.google-analytics.com/ga.js", function() {
+		var tracker = _gat._getTracker("UA-25975548-1");
+		tracker._trackPageview();
+	});
 });
