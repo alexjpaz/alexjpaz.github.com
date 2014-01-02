e519619ebc42590362498fcc445aef4c39689eba
Alex Paz <alexander.j.paz@ngc.com>
2014-01-02 13:54:49 -0500

    modifiying layout

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,9 +1,10 @@
 (function(){
-	// Any other scripts loaded here
-
 	// Google Analytics
 	head.js("http://www.google-analytics.com/ga.js", function() {
 		var tracker = _gat._getTracker("UA-25975548-1");
 		tracker._trackPageview();
 	});
+
+	head.js('//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js','//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js');
+
 })();
